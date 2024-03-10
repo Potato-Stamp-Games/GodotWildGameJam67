c2c6b1822535605ff48fb9f628cc6c4df0c6b171
Banhail <44350226+Banhail@users.noreply.github.com>
2024-03-10 15:43:11 -0500

    Base Parasite Animation
    Base Parasite Animation Added

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
 
+func _ready():
+	$Sprite2D.play("new_animation")
+	
+
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed
