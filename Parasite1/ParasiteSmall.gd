277ad225d1349b8cbae6022060f3a531ef87ae3a
Banhail <44350226+Banhail@users.noreply.github.com>
2024-03-15 10:36:18 -0500

    Parasite Death/Nail Gun
    -Nailgun Shoots at enemys
    -Enemys die from 2 hits of nailgun
    -Seperated the player with suit script from the player without suit script (Causes a issue in the loadout menu trying to auto use a weapon)

--- Parasite1/ParasiteSmall.gd
+++ Parasite1/ParasiteSmall.gd
@@ -2,13 +2,31 @@ extends CharacterBody2D
 
 @export var movement_speed = 40.0
 @export var hp = 10
+@export var knockback_recovery = 3.5
+var knockback = Vector2.ZERO
+
 @onready var player = get_tree().get_first_node_in_group("player")
+
+signal remove_from_array(object)
 
 func _ready():
 	$Sprite2D.play("new_animation")
 	
 
 func _physics_process(_delta):
+	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed
+	velocity += knockback
 	move_and_slide()
+
+func death():
+	emit_signal("remove_from_array", self)
+	queue_free()
+
+func _on_hurt_box_hurt(damage, angle, knockback_amount):
+	hp -= damage
+	knockback = angle * knockback_amount
+	if hp <= 0:
+		emit_signal("remove_from_array", self)
+		death()
