09d9dd31ca1575ae36e447529f3fb032dd2314c0
Banhail <44350226+Banhail@users.noreply.github.com>
2024-03-16 14:01:37 -0500

    XP Paws
    -Xp Paws drop on enemy death

--- Parasite1/ParasiteSmall.gd
+++ Parasite1/ParasiteSmall.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 @export var movement_speed = 40.0
 @export var hp = 10
 @export var knockback_recovery = 3.5
+@export var experience = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var loot_base = get_tree().get_first_node_in_group("loot")
 
+var exp_paw = preload("res://Utility/experience_paw.tscn")
+var EXPERIENCE_PAW = preload("res://Utility/experience_paw.tscn")
 signal remove_from_array(object)
 
 func _ready():
@@ -22,6 +26,10 @@ func _physics_process(_delta):
 
 func death():
 	emit_signal("remove_from_array", self)
+	var new_expPaw = EXPERIENCE_PAW.instantiate()
+	new_expPaw.global_position = global_position
+	new_expPaw.experience = experience
+	loot_base.call_deferred("add_child", new_expPaw)
 	queue_free()
 
 func _on_hurt_box_hurt(damage, angle, knockback_amount):
